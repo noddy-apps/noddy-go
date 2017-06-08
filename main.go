b55e668674a92d1d0dd9419128bcf258f5b06e69
Tim Langford <timothy.langford@oracle.com>
2017-06-08 15:08:22 +0100

    Gave noddy a home.

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "I am just a little Noddy... no friends... no bone...\n")
+
+	hostname, _ := os.Hostname()
+
+	var msg1 = "I am just a little Noddy... no friends... no bone...\n"
+	var msg2 = "I just sit here and wait in my little " + hostname + " home...\n"
+	fmt.Fprintf(w, msg1+msg2)
 }
 
 func main() {
